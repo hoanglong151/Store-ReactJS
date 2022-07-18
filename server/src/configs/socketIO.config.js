6fca9ce85f7487979a66ebca146867f64bd57d27
hoanglong151 <nguyenphamhoanglong2000@gmail.com>
2022-07-18 14:52:38 +0700

    make it better

--- server/src/configs/socketIO.config.js
+++ server/src/configs/socketIO.config.js
@@ -7,7 +7,7 @@ const { Server } = require("socket.io");
 // });
 
 const io = new Server({
-  cors: "https://cleanfood.vercel.app/",
+  cors: "https://cleanfood-yq8rp9dc9-hoanglong151.vercel.app/",
 });
 
 io.on("connection", (socket) => {
